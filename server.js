ffbc2e3e08d8c18e8a08a1c6ddb294a30626773d
mayankja <mayankjadam74@gmail.com>
2021-06-03 16:29:16 +0530

    Deploying to Heroku

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + './dist/ang-assignment'));
+app.use(express.static(__dirname + '/dist/ang-assignment'));
 
 // app.use(requireHTTPS);
 // function requireHTTPS(req, res, next) {
